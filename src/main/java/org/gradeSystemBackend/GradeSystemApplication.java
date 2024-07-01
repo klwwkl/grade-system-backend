34eb6fa82ff72d94c5c837e0c01508b060ba6347
klwwkl <2432875209@qq.com>
2024-07-01 13:50:41 +0800

    项目基础结构搭建

---
+++ src/main/java/org/gradeSystemBackend/GradeSystemApplication.java
@@ -0,0 +1,13 @@
+package org.gradeSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSystemApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(GradeSystemApplication.class, args);
+    }
+}
